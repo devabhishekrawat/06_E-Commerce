7a401ab2df9d544a9c1118a49ecb98671b2ef117
devabhishekrawat <abhishek.rawat@axeno.co>
2026-08-16 15:29:50 +0530

    implement the home page with the dynamic rendering of the products

---
+++ js/auth.js
@@ -0,0 +1,47 @@
+const loginForm = document.querySelector("#login-form");
+
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+
+const loginError = document.querySelector("#login-error");
+
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+    const correctEmail = "admin@example.com";
+    const correctPassword = "Admin@123";
+    if (email !== correctEmail) {
+
+        loginError.textContent =
+            "Email address is incorrect.";
+
+        return;
+    }
+
+    if (password !== correctPassword) {
+
+        loginError.textContent =
+            "You did not enter the correct password.";
+
+        passwordInput.focus();
+
+        return;
+    }
+
+
+    const user = {
+        email: email,
+        isLoggedIn: true
+    };
+
+    sessionStorage.setItem(
+        "user",
+        JSON.stringify(user)
+    );
+
+    loginError.textContent = "";
+    alert("Login successful!");
+    window.location.href = "../index.html"
+});
